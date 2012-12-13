8c6909e5a939dfe7d96f3bca4ad9f072bc487034
Jeffrey Heifetz <jheifetz@rim.com>
2012-12-13 11:37:21 -0500

    Updates the test-suite command for better reporting to the console. Also adds two more apps to the suite.
        Reviewed By: Nukul Bhasin <nbhasin@rim.com>
        Tested By: Hassan Ahmad <haahmad@rim.com>

--- build/test-suite.js
+++ build/test-suite.js
@@ -14,56 +14,130 @@
  * limitations under the License.
  */
 module.exports = function (done, custom) {
-    var jasmine = require('jasmine-node'),
-        verbose = false,
-        colored = false,
-        specs = __dirname + "/../" + (custom ? custom : "test/test-suite/test"),
+    var sys = require('sys'),
+        path = require('path'),
+        TEST_APP_PATH = path.normalize(__dirname + "/../test/test-app/"),
+        TEST_SUITE_PATH = path.normalize(__dirname + "/../test/test-suite/"),
+        TEST_SUITE_APPS_PATH = path.join(TEST_SUITE_PATH, "Apps"),
+        TEST_SUITE_OUTPUT_PATH = path.join(TEST_SUITE_PATH, "data", "output"),
+        utils = require('./build/utils'),
+        CLITest = require(TEST_SUITE_PATH + '/test/CLITest'),
+        DeployTest = require(TEST_SUITE_PATH + '/test/DeployTest'),
+        reporter = require(TEST_SUITE_PATH + 'test/reporter'),
+        settings = require(__dirname + '/../test-runner.json'),
+        wrench = require("wrench"),
+        queue,
+        finalResults = {
+            suites: {
+                length: 0
+            },
+            results: {
+                totalCount: 0,
+                passedCount: 0,
+                failedCount: 0,
+                skipped: false,
+                items_: []
+            }
+        },
         key;
 
-    //HACK: this should be  taken out if our pull request in jasmine is accepted.
-    jasmine.Matchers.prototype.toThrow = function (expected) {
-        var result = false,
-            exception,
-            not = this.isNot ? "not " : "";
-
-        if (typeof this.actual !== 'function') {
-            throw new Error('Actual is not a function');
+    queue = [
+        {
+            name: "Disable Web Security Tests",
+            input: TEST_SUITE_APPS_PATH + "/DisableWebSecurity/",
+            output: TEST_SUITE_OUTPUT_PATH + "/device/DisableWebSecurity.bar"
+        }, {
+            name: "Subfolder Content Page Tests",
+            input: TEST_SUITE_APPS_PATH + "/SubfolderStartupPage/",
+            output: TEST_SUITE_OUTPUT_PATH + "/device/SubfolderStartupPage.bar"
+        }, {
+            name: "Wildcard (*) Access Tests",
+            input: TEST_SUITE_APPS_PATH + "/WildcardDomain/",
+            output: TEST_SUITE_OUTPUT_PATH + "/device/WildcardDomain.bar"
+        }, {
+            name: "WebWorks Test App",
+            input: TEST_SUITE_APPS_PATH + "/wwTest/",
+            output: TEST_SUITE_OUTPUT_PATH + "/device/wwTest.bar"
         }
-        try {
-            this.actual();
-        } catch (e) {
-            exception = e;
-        }
-        if (exception) {
-            if (typeof expected === 'function') {
-                result = expected(exception);
-            }
-            else {
-                result = (expected === jasmine.undefined || this.env.equals_(exception.message || exception, expected.message || expected));
-            }
-        }
+    ];
 
-        this.message = function () {
-            if (exception && (expected === jasmine.undefined || !this.env.equals_(exception.message || exception, expected.message || expected))) {
-                return ["Expected function " + not + "to throw", expected ? expected.message || expected : "an exception", ", but it threw", exception.message || exception].join(' ');
-            } else {
-                return "Expected function to throw an exception.";
+    //Prepare WebWorks Test App
+    utils.copyFile(path.join(TEST_APP_PATH, "config.xml"), path.join(TEST_SUITE_APPS_PATH, "wwTest"));
+    wrench.rmdirSyncRecursive(path.join(TEST_SUITE_APPS_PATH, "wwTest", "spec"), true);
+    wrench.copyDirSyncRecursive(path.join(__dirname,"..", "test", "functional", "automatic"), path.join(TEST_SUITE_APPS_PATH,"wwTest", "spec"));
+
+
+    function processItem (queueItem, done) {
+
+        wrench.rmdirSyncRecursive(TEST_SUITE_OUTPUT_PATH, true);
+        wrench.mkdirSyncRecursive(TEST_SUITE_OUTPUT_PATH, "0755");
+
+        var cliTest2;
+        cliTest2 = new CLITest(settings.packager);
+        cliTest2.addOption(queueItem.input);
+        cliTest2.addOption("-d");
+        cliTest2.addOption("-o", TEST_SUITE_OUTPUT_PATH);
+        cliTest2.run(function () {
+            var reqData = [],
+                flag = false,
+                dt,
+                start;
+
+            try {
+
+                dt = new DeployTest(queueItem.output);
+                dt.listen(function (request) {
+                    //console.log(">>>>>>>> Data Received", request);
+                    if (request.status === 'finished') {
+                        //Update finalResults
+                        finalResults.suites.length += request.data.suites.length;
+                        finalResults.results.totalCount += request.data.results.totalCount;
+                        finalResults.results.passedCount += request.data.results.passedCount;
+                        finalResults.results.failedCount += request.data.results.failedCount;
+                        finalResults.results.items_.push(request.data.results.items_);
+                        //Print Whitespace
+                        sys.print('\n');
+                        sys.print("FINISHED " + queueItem.name);
+                        sys.print('\n');
+                        sys.print('\n');
+
+                        dt.terminate(function () {
+                            done();
+                        });
+                    } else if (request.status === "SuiteResults") {
+                        hackSummaryReporter.reportSuiteResults(request.data);
+                    } else if (request.status === "SpecResults") {
+                        hackSummaryReporter.reportSpecResults(request.data);
+                    } else if (request.status === "RunnerStarting") {
+                        sys.print("STARTING " + queueItem.name);
+                        sys.print('\n');
+                    }
+                });
+                dt.startServer();
+                dt.deploy();
+            } catch(e) {
+                dt.terminate(function () {
+                    throw e;
+                });
             }
-        };
+        });
+    }
 
-        return result;
-    };
+    var hackSummaryReporter = new reporter.TerminalReporter();
 
-    for (key in jasmine) {
-        if (Object.prototype.hasOwnProperty.call(jasmine, key)) {
-            global[key] = jasmine[key];
+    function processQueue() {
+        var queueItem;
+        if (queue.length) {
+            queueItem = queue.shift();
+            processItem(queueItem, processQueue);
+        } else {
+            hackSummaryReporter.reportRunnerResults(finalResults);
+            sys.print("TEST SUITE COMPLETED");
+            sys.print('\n');
         }
     }
 
-    jasmine.executeSpecsInFolder(specs, function (runner, log) {
-        var failed = runner.results().failedCount === 0 ? 0 : 1;
-        setTimeout(function () {
-            (typeof done !== "function" ? process.exit : done)(failed);
-        }, 10);
-    }, verbose, colored);
+    sys.print("STARTING TEST SUITE");
+    sys.print('\n');
+    processQueue();
 };
